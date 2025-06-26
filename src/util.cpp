1eae5e58b5b2dcfeb9d9887a0e42764f30665bab
Patadass <ajdinskiivan1@gmail.com>
2025-06-26 16:54:24 +0200

    Change logger::log(string,int)
    Add date and time option for logger::log(string,int)
    where int is TIME_ON or TIME_OFF

--- src/util.cpp
+++ src/util.cpp
@@ -1,17 +1,26 @@
 #include "util.h"
 #include <fstream>
+#include <ctime>
 
 using namespace std;
 
 bool logger::erase = true;
 
-void logger::write(string line){
+void logger::write(string line, int log_time){
     ofstream stream;
     if(erase){
         stream.open(LOG_FILE_PATH, ios::trunc);
         logger::erase = false;
     }else{
         stream.open(LOG_FILE_PATH, ios::app);
+    }
+
+    if(log_time == TIME_ON){
+        time_t timestamp;
+        char out[30];
+        time(&timestamp);
+        strftime(out, 30, "%F %H:%M:%S ", localtime(&timestamp));
+        stream<<out;
     }
     stream<<line<<"\n";
     stream.close();
