d44d988f8418eb038be7fe49bf0bf909ae0c2ed1
Patadass <ajdinskiivan1@gmail.com>
2025-06-26 16:53:16 +0200

    Add time to logger::log(string,int)

--- src/util.h
+++ src/util.h
@@ -4,12 +4,15 @@
 #ifndef UTIL
 #define UTIL
 
+#define TIME_ON 1
+#define TIME_OFF 0
+
 const std::string LOG_FILE_PATH = "./log";
 
 struct logger{
 private:
     static bool erase;
 public:
-    static void write(std::string line);
+    static void write(std::string line, int log_time=TIME_OFF);
 };
 #endif
