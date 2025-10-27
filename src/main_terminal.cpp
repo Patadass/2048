d19c7a062c999256bf14867301c105648b59b96a
Patadass <ajdinskiivan1@gmail.com>
2025-10-27 17:08:34 +0100

    add drawing board
    chaged input

--- src/main_terminal.cpp
+++ src/main_terminal.cpp
@@ -6,16 +6,16 @@
 using namespace std;
 
 int get_dir_from_string(string s){
-    if(s[0] == 'u' || s[0] == 'U'){
+    if(s[0] == 'w' || s[0] == 'W'){
         return DIR_UP;
     }
-    if(s[0] == 'r' || s[0] == 'R'){
+    if(s[0] == 'd' || s[0] == 'D'){
         return DIR_RIGHT;
     }
-    if(s[0] == 'd' || s[0] == 'D'){
+    if(s[0] == 's' || s[0] == 'S'){
         return DIR_DOWN;
     }
-    if(s[0] == 'l' || s[0] == 'L'){
+    if(s[0] == 'a' || s[0] == 'A'){
         return DIR_LEFT;
     }
     return DIR_UP;
@@ -34,7 +34,7 @@ int main(){
     while(!gb.all_set()){
         string s_dir;
         cout<<endl;
-        cout<<"(u)p (r)ight (d)own (l)eft (q)uit"<<endl;
+        cout<<"(w)up (d)right (s)down (a)left (q)uit"<<endl;
         cin>>s_dir;
         if(s_dir[0] == 'q' || s_dir[0] == 'Q'){
             return 0;
@@ -42,11 +42,12 @@ int main(){
         graphics::clear_screen();
         int dir = get_dir_from_string(s_dir);
         if(!gb.make_move(dir)){
-            //graphics::clear_screen();
+            graphics::draw_score(gb);
             graphics::draw_board(gb);
             continue;
         }
         gb.set_random(2);
+        graphics::draw_score(gb);
         graphics::draw_board(gb);
     }
 }
