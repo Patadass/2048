3a714d168556a84f040c6494cd66ac11ec97ba33
Patadass <ajdinski@pm.me>
2026-01-29 09:41:19 +0100

    added prompt before quiting
    gives a prompt before quiting the program
    moved checking of s_dir == 'q' inside of switch

--- src/main_curses.cpp
+++ src/main_curses.cpp
@@ -29,30 +29,54 @@ int main(){
             wrefresh(win);
         }
         int s_dir = getch();
-        if(s_dir == 'q' || s_dir == 'Q'){
-            break;
-        }
+
         int dir;
         switch(s_dir){
             case 'a':        
+            case 'A':
                 dir = DIR_LEFT;
             break;
             case 'd':
+            case 'D':
                 dir = DIR_RIGHT;
             break;
             case 's':
+            case 'S':
                 dir = DIR_DOWN;
             break;
             case 'w':
+            case 'W':
                 dir = DIR_UP;
             break;
             case 'l':
+            case 'L':
                 dir = DIR_LOG;
             break;
+            case 'q':
+            case 'Q':
+                attron(COLOR_PAIR(7));
+                mvprintw(graphics::board_row + 3 + (graphics::field_height * 4)
+                        , graphics::board_column - 5 + (graphics::field_width * 2)
+                        , "QUIT? [y/n]");
+                refresh();
+                s_dir = getch();
+                attroff(COLOR_PAIR(7));
+                if(s_dir == 'y' || s_dir == 'Y'){
+                    graphics::end();
+                    return 0;
+                }
+                attron(COLOR_PAIR(9));
+                mvprintw(graphics::board_row + 3 + (graphics::field_height * 4)
+                        , graphics::board_column - 5 + (graphics::field_width * 2)
+                        , "            ");
+                refresh();
+                attroff(COLOR_PAIR(9));
+                dir = DIR_UNDEF;
             default:
                 dir = DIR_UNDEF;
             continue;
         }
+
         if(dir == DIR_LOG){
             if(win != nullptr){
                 logger::ushow_log();
@@ -65,9 +89,11 @@ int main(){
             win = (WINDOW*) logger::show_log();
             continue;
         }
+
         if(!gb.make_move(dir)){
             continue;
         }
+
         graphics::clear_screen();
         gb.set_random(2);
         graphics::draw_board(gb);
